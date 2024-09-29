4697f48e7e1d820ae7ff098f47e08983ca5c790e
Abhishek Sunil <abhisheksunil92@gmail.com>
2024-09-29 18:54:44 +0530

    formatting

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	server := server.NewServer()
-  log.Println("Starting server")
+  	log.Println("Starting server")
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
